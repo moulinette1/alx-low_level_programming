c75ff6c2d5b00f8ea43d480d2cf33d21bfa8085c
Mouna_Elghazi <mouna.elghazi99@gmail.com>
2023-07-14 06:44:59 -0700

    5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'Print lowercase except q and e'
+ * Return: always 0
+ */
+int main(void)
+{
+int n;
+
+for (n = 0; n <= 9; n++)
+{
+	printf("%d", n);
+}
+printf("\n");
+return (0);
+}
