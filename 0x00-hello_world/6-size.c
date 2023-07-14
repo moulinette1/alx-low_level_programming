30f6fb9c9c83da2814298653d254086c320dc66b
Mouna_Elghazi <mouna.elghazi99@gmail.com>
2023-07-13 23:46:16 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,5 +10,5 @@ int main(void)
 	printf("Size of a long int: %d byte(s)", sizeof(long int));
 	printf("Size of a long long int: %d byte(s)", sizeof(long long int));
 	printf("Size of a float: %d byte(s)", sizeof(float));
-	return (0)
+	return (0);
 }
